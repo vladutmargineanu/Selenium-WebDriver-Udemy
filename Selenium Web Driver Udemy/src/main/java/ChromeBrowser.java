d8e408742a42f286b48661dbbfb6be249d88e8ea
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-04-12 19:31:02 +0300

    Update ChromeBrowser.java

--- Selenium Web Driver Udemy/src/main/java/ChromeBrowser.java
+++ Selenium Web Driver Udemy/src/main/java/ChromeBrowser.java
@@ -15,13 +15,13 @@ public class ChromeBrowser {
         driver.get("http://facebook.com"); // URL in the browser
 
         // Locator id - email
-        // driver.findElement(By.id("email")).sendKeys("This is my first code");
+        driver.findElement(By.id("email")).sendKeys("This is my first code");
 
         // Locator name - password
-        // driver.findElement(By.name("pass")).sendKeys("123456");
+        driver.findElement(By.name("pass")).sendKeys("123456");
 
         // tag a => link text
-        // driver.findElement(By.linkText("Ai uitat parola?")).click();
+        driver.findElement(By.linkText("Ai uitat parola?")).click();
 
         // Use Selector CSS
         // 1. For email
@@ -31,13 +31,10 @@ public class ChromeBrowser {
         driver.findElement(By.xpath("//*[@id=\"pass\"]")).sendKeys("123456");
 
         // 3. For Forgot Password?
-        // driver.findElement(By.xpath(".//a[contains(text(),'Forgot Password')]")).click();
-        // driver.findElement(By.xpath(".//div[@class='_6ltj']/a")).click();
+        driver.findElement(By.xpath(".//a[contains(text(),'Forgot Password')]")).click();
+        driver.findElement(By.xpath(".//div[@class='_6ltj']/a")).click();
 
-        // driver.findElement(By.cssSelector("#email")).sendKeys("vladvlad");
-
-       // driver.findElement(By.cssSelector())
-
+        driver.findElement(By.cssSelector("#email")).sendKeys("vladvlad");
 
     }
 }
