49c494aff1d2a48b79b77682e756fbdbc7c11ae2
Vladut Margineanu <vladut.margineanu@gmail.com>
2021-04-12 19:33:35 +0300

    Update RedDiff.java

--- Selenium Web Driver Udemy/src/main/java/RedDiff.java
+++ Selenium Web Driver Udemy/src/main/java/RedDiff.java
@@ -13,7 +13,6 @@ public class RedDiff {
         driver.get("https://www.rediff.com/"); // URL in the browser
 
         // Regex Expression
-
         driver.findElement(By.cssSelector("a[title*='Sign in'")).click();
 
         // driver.findElement(By.xpath("//*[contains(@id,'login')]")).sendKeys("vlad"); // error
@@ -21,7 +20,5 @@ public class RedDiff {
         driver.findElement(By.cssSelector("input#password")).sendKeys("pass");
 
         driver.findElement(By.xpath("//input[contains(@name,'procee')]")).click();
-
-
     }
 }
